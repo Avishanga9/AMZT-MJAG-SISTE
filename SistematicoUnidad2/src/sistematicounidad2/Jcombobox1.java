644014876b48188101db5bcdd12ed3aceebdfa69
Markalvarez512 <111943853+Markalvarez512@users.noreply.github.com>
2022-10-17 08:29:15 -0600

    Add files via upload
    SI

---
+++ SistematicoUnidad2/src/sistematicounidad2/Jcombobox1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistematicounidad2;
+
+/**
+ *
+ * @author isi7
+ */
+class Jcombobox1 {
+
+    static Object getSelectedItem() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ SistematicoUnidad2/src/sistematicounidad2/selectedValue.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistematicounidad2;
+
+/**
+ *
+ * @author isi7
+ */
+class selectedValue {
+    
+}
